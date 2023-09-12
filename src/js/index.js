ab7aa8d6bb86754f2fc56ec7df36715e9496972a
Pavel <pavel.norko.91@mail.ru>
2023-09-12 16:49:26 +0300

    add class Card

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 import '../style/style.scss'
-console.log("Hello World!");
+import { counterId, Card } from './classCard';
+
+
+const card1 = new Card('../images/card8.jpg', 'Телевизор LG 50UQ75006LF', '1296.00');
+
+card1.render()
+
+const card2 = new Card('../images/card8.jpg', 'ыфвфвфвфв', '325.12');
+
+card2.render()
+
+const card3 = new Card('../images/card8.jpg', '11уцуйуйцуйуйу', '22.12');
+card3.render()
+const card4 = new Card('../images/card8.jpg', 'щщдтртртр', '1.12');
+card4.render()
+const card5 = new Card('../images/card8.jpg', 'мммсмиси', '11111.12');
+card5.render()
